b474b5b06cf952499370a6654d8d89dd0335cfd2
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-03-24 18:45:38 +0100

    feat: create BeerDTO

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDTO.java
@@ -0,0 +1,33 @@
+package guru.springframework.msscbeerservice.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDTO {
+    private UUID id;
+    private Integer version;
+
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModified;
+
+    private String beerName;
+
+    private BeerStyleEnum beerStyle;
+
+    private Long upc;
+
+    private BigDecimal price;
+
+    private Integer quantityOnHand;
+
+}
