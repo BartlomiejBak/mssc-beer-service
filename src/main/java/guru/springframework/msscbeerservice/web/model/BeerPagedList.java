3a411843260a599a5e9a9dda86b38db6f2f1918b
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-03-26 17:34:18 +0100

    feat: add BeerPagedList

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerPagedList.java
@@ -0,0 +1,18 @@
+package guru.springframework.msscbeerservice.web.model;
+
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public class BeerPagedList extends PageImpl<BeerDTO> {
+
+    public BeerPagedList(List<BeerDTO> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public BeerPagedList(List<BeerDTO> content) {
+        super(content);
+    }
+}
