f56e4a15029ab39d085f2dd8dcafe84ca16377d0
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-03-28 19:07:02 +0200

    test: add BeerRepository

---
+++ src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,10 @@
+package guru.springframework.msscbeerservice.repositories;
+
+import guru.springframework.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+}
